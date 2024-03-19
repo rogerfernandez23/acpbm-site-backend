82c30f99f78ab9f45e976c8fdf0039d1aa20b665
rogerfernandez23 <rogerio.fernandes2212@gmail.com>
2024-03-19 20:39:09 -0300

    last-day

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const app = require('./app.js');
+const { Router } = require('express');
+
+const routes = Router();
 
 const port = process.env.PORT || 8080;
 
 app.listen(port);
+
+routes.get('/', (req, res) => {
+    return res.json({sucess: 'Servidor iniciado!! 🚀'})
+})
+
+
