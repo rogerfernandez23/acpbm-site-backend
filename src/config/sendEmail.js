f69ec37251b9d4528d54a06b2e564a206ff44c13
rogerfernandez23 <rogerio.fernandes2212@gmail.com>
2024-04-10 21:48:07 -0300

    restart-changes

--- src/config/sendEmail.js
+++ src/config/sendEmail.js
@@ -2,13 +2,10 @@ const { exec } = require('child_process');
 
 const send = (name, email) => {
 
-  const pathScript = require('../integrations/email_send.py');
-
   const nameUser = `"${name}"`;
   const emailUser = `"${email}"`;
-  const path = `"${pathScript}"`;
 
-  exec(`python ${path} ${nameUser} ${emailUser}`, (err, stdout, stderr) => {
+  exec(`python ../integrations/email_send.py ${nameUser} ${emailUser}`, (err, stdout, stderr) => {
     if (err) {
       console.error(`Erro ao enviar o e-mail: ${err}`)
       return
